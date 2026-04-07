90d64ffa53f2fa3c5a60330732e6e3c94e76dffa
ginseng <joao.lsantos2021@gmail.com>
2026-04-07 16:25:46 -0300

    try 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import AdminDashboardPage from "./pages/AdminDashboardPage";
 import CaseDetailPage from "./pages/CaseDetailPage";
 import AdminTipsPage from "./pages/AdminTipsPage";
 import PublicGuidePage from "./pages/PublicGuidePage";
-import AdminNewsPage from "./pages/AdminNewsPage";
-import AdminNewsEditorPage from "./pages/AdminNewsEditorPage";
-import NewsDetailPage from "./pages/NewsDetailPage";
 import RedeInformaPage from "./pages/RedeInformaPage";
 import NewsDetailPage from "./pages/NewsDetailPage";
+import AdminNewsPage from "./pages/AdminNewsPage";
+import AdminNewsEditorPage from "./pages/AdminNewsEditorPage";
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,17 +19,15 @@ export default function App() {
         <Route path="/" element={<RedeAlertaHomepage />} />
         <Route path="/cadastrar" element={<CreateCasePage />} />
 
-        {/* 🔒 ROTAS INTERNAS */}
         <Route path="/colaborador" element={<AdminDashboardPage />} />
         <Route path="/colaborador/login" element={<AdminLoginPage />} />
         <Route path="/colaborador/informacoes" element={<AdminTipsPage />} />
         <Route path="/colaborador/noticias" element={<AdminNewsPage />} />
         <Route path="/colaborador/noticias/nova" element={<AdminNewsEditorPage />} />
         <Route path="/colaborador/noticias/:id/editar" element={<AdminNewsEditorPage />} />
-        {/* 🌐 PÚBLICO */}
+
         <Route path="/caso/:id" element={<CaseDetailPage />} />
         <Route path="/guia" element={<PublicGuidePage />} />
-        <Route path="/informacoes/:slug" element={<NewsDetailPage />} />
         <Route path="/informacoes" element={<RedeInformaPage />} />
         <Route path="/informacoes/:slug" element={<NewsDetailPage />} />
       </Routes>
